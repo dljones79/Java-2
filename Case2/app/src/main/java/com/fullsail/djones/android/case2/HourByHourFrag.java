35c5fa8060b1b0cbf14f54ef58f6c678fa9694ed
David Jones <dljones79@fullsail.edu>
2014-08-29 21:17:57 -0500

    All Case 2 Items working.

---
+++ Case2/app/src/main/java/com/fullsail/djones/android/case2/HourByHourFrag.java
@@ -0,0 +1,38 @@
+package com.fullsail.djones.android.case2;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class HourByHourFrag extends Fragment {
+
+
+    public static HourByHourFrag newInstance() {
+        HourByHourFrag fragment = new HourByHourFrag();
+        return fragment;
+    } // end of HourByHourFrag newInstance()
+
+    public HourByHourFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hour_by_hour, container, false);
+    }
+
+
+}
---
+++ Case2/app/src/main/java/com/fullsail/djones/android/case2/WeeklyForcastFrag.java
@@ -0,0 +1,38 @@
+package com.fullsail.djones.android.case2;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class WeeklyForcastFrag extends Fragment {
+
+
+    public static WeeklyForcastFrag newInstance() {
+        WeeklyForcastFrag fragment = new WeeklyForcastFrag();
+        return fragment;
+    } // end of WeeklyForcastFrag newInstance()
+
+    public WeeklyForcastFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weekly_forcast, container, false);
+    }
+
+
+}
